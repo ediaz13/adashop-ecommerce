30987e133f701a18afbc7965884e4e2acb9ad7f1
ediaz13 <diazemanuel27@gmail.com>
2022-05-02 00:09:01 -0300

    30. Message & Loader Components

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,7 +3,7 @@ import { PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_REQUEST, 
     PRODUCT_LIST_FAIL } from "../constants/productConstants"
 
-export const listProducts () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST})
 
@@ -17,8 +17,8 @@ export const listProducts () => async (dispatch) => {
         dispatch({
             type: PRODUCT_LIST_FAIL,
             payload: error.response && error.response.data.message 
-            ? error.response.data.message
-            :error.message,
+            ? error.response.data.message 
+            : error.message
         })
     }
 }
