63ee85498dcb4262e3faa4c956a20252af2ed2f2
ediaz13 <diazemanuel27@gmail.com>
2022-06-05 22:04:55 -0300

    40. Generate a JSON Web Token

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,36 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
-import colors from 'colors'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import productRoutes from './routes/productRoutes.js'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-dotenv.config()
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
-connectDB()
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
+app.use(express.json())
 
-app.use('/api/products', productRoutes)
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.use(notFound)
-app.use(errorHandler)
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000
+app.use(notFound);
+app.use(errorHandler);
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port: ${PORT}`.yellow.underline))
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port: ${PORT}`.yellow
+      .underline
+  )
+);
