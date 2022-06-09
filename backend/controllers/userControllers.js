747060bfa532e23665ee002d21535f1f9f0b1956
ediaz13 <diazemanuel27@gmail.com>
2022-06-09 19:55:18 -0300

    41. Custom Authentication Middleware. First part.

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -24,4 +24,13 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser }
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    //const user = awit User,findById(req.user._id)
+    res.send('Success')
+    
+  })
+
+export { authUser, getUserProfile }
